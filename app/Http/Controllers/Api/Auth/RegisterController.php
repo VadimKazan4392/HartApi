3b2bc375fa593da8d898de4043f68f53dd65d22c
vladlengil <funkylen@gmail.com>
2021-04-05 12:07:00 +0300

    Добавление UserRegisterRequest. Рефакторинг register метода

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\UserRegisterRequest;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
@@ -12,36 +13,25 @@ use Illuminate\Support\Str;
 
 class RegisterController extends Controller
 {
-    public function register(Request $request) 
+    public function register(UserRegisterRequest $request)
     {
         try {
-            $validator = Validator::make($request->all(), [
-                'name' => 'string|max:20',
-                'email' => 'required|email|max:255|unique:users',
-                'password' => 'required|min:6|max:255|confirmed',
-            ]);
-            
-            if($validator->fails()) {
-                return response([
-                    'error' => $validator->errors()->all()
-                ], 422);
-            }
+            $user = new User();
 
-            $request['password'] = Hash::make($request['password']);
-            $request['remember_token'] = Str::random(10);
-            $user = User::create($request->toArray());
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->remember_token = Str::random(10);
 
-            return response()->json([
-                'status_code' => 200,
-                'message' => 'Registration successfull',
-            ]);
+            $user->save();
+
+            return response()->json($user);
 
         } catch (Exception $error) {
             return response()->json([
-                'status_code' => 500,
                 'message' => 'Error in Registration',
                 'error' => $error,
-            ]);
+            ], 500);
         }
     }
 }
